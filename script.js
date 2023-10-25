3e4193a43f322c03f8ab25bfe46b3170ead8960a
julldii <148975566+julldii@users.noreply.github.com>
2023-10-25 21:16:44 +0800

    script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+const myButton = document.getElementById('myButton');
+
+myButton.addEventListener('click', function() {
+    // Code to execute when the button is clicked
+    alert('Button clicked!');
+});
