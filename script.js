d6e17633bc425b5eec3ea5a731db121c6a106b01
julldii <148975566+julldii@users.noreply.github.com>
2023-10-25 21:32:09 +0800

    script.js

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ myButton.addEventListener('click', function() {
     // Code to execute when the button is clicked
     alert('Button clicked!');
 });
+<!DOCTYPE html>
+<html>
+<head>
+    <!-- Your HTML head content here -->
+    <script src="https://raw.githubusercontent.com/julldii/math-problem-solver./main/script.js"></script>
+</head>
+<body>
+    <h1>My Web Page</h1>
+    <button id="myButton">Click me</button>
+</body>
+</html>
